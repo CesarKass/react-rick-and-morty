91634af7246265df28dae00572ae4edc1e6a8e71
CesarKass <cesar.acu@hotmail.com>
2023-08-25 10:21:24 -0600

    gb-pages responsive fix

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -9,14 +9,13 @@ function Character({ character }) {
 
                     <div className="card text-bg-dark mb-3">
                         <div className="row">
-                            <div className="col-sm-4 col-md-4 col-lg-4">
+                            <div className="col-4 col-sm-4 col-md-4 col-lg-4">
                                 <img src={character.image} alt={character.name} className="img-fluid rounded-start" style={{ height: '110px' }} />
                             </div>
-                            <div className="col-sm-8 col-md-8 col-lg-8">
+                            <div className="col-8 col-sm-8 col-md-8 col-lg-8">
                                 <div className="card-body p-0 pt-2 px-1">
                                     <h5 className="card-title">{character.name} </h5>
                                     <p>{character.origin.name}</p> 
-
                                 </div>
                             </div>
                         </div>
